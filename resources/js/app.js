ceb3dd8aec792e1d4190c72726bf93df273c4e10
JoernT <joern.turner@betterform.de>
2015-05-28 10:02:36 +0200

    init in progress

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,8 @@
 * put application-specific JavaScript code
 */
 'use strict';
+
+$('#insert-zone').on("click",function(){
+    console.log("insert zone");
+    fluxProcessor.dispatch("insert-zone");
+});
